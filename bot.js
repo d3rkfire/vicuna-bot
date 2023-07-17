a01a1a140946720559f4e81cf5d4398b358cb9d9
Vathna <c.neak@yahoo.com.au>
2023-07-17 09:49:50 +0700

    removed inline setrole

--- bot.js
+++ bot.js
@@ -12,27 +12,13 @@ const TelegramBot = require("node-telegram-bot-api")
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true})
 
 // Set Role
-bot.onText(/^\/setrole$/, (message, _) => {
+bot.onText(/^\/setrole/, (message, _) => {
     bot.sendMessage(message.chat.id, responses.en.setrole.describe)
     roleRequestQueue.push(message.chat.id)
 })
-bot.onText(/^\/setrole (.+)$/, (message, match) => {
-    const roleDescription = match[1].replace(/^\"/, "").replace(/\"$/, "")
-    const roleFilepath = "./roles/" + message.chat.id
-    fs.writeFile(roleFilepath, roleDescription, (error) => {
-        if (error) {
-            console.log(error)
-            bot.sendMessage(message.chat.id, responses.en.setrole.fail)
-        } else {
-            const response = responses.en.setrole.success + roleDescription
-            bot.sendMessage(message.chat.id, response)
-        }
-        roleRequestQueue.splice(roleRequestQueue.indexOf(message.chat.id), 1)
-    })
-})
 
 // Reset Role
-bot.onText(/^\/resetrole$/, (message, _) => {
+bot.onText(/^\/resetrole/, (message, _) => {
     const roleFilepath = "./roles/" + message.chat.id
     fs.unlink(roleFilepath, (error) => {
         if (error) console.log(error)
@@ -70,7 +56,7 @@ bot.onText(/^[^\/].*/, (message, _) => {
             
             const promptRequest = JSON.stringify({
                 "prompt": "### Instruction:\n" + role + "\n\n### Human:\n" + message.text + "\n\n### Assistant:\n",
-                "max_new_tokens": 1024,
+                "max_new_tokens": 2048,
                 "temparature": 0.7,
                 "repetition_penalty": 1.2,
                 "min_length": 10,
